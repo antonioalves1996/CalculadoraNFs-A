3820df120f2dce1dea1738560494211897d2366a
antonioalves1996 <168119183+antonioalves1996@users.noreply.github.com>
2024-05-26 20:04:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,72 +1,71 @@
- document.getElementById('calcularBtn').addEventListener('click', function() {
-        calcularTaxa();
-    });
-
-    function calcularTaxa() {
-        let valorNF = parseFloat(document.getElementById('ValorNF').value.replace(/\./g, '').replace(',', '.'));
-
-        if (isNaN(valorNF)) {
-            alert('Por favor, insira um valor numérico válido.');
-            return;
+function formatarValor(valor) {
+            // Formatar o valor para exibir milhares com ponto e centavos com vírgula
+            let partes = valor.toFixed(2).split('.');
+            let inteiro = parseInt(partes[0]).toLocaleString('pt-BR');
+            let decimal = partes[1];
+            return `${inteiro},${decimal}`;
         }
 
-        const tetoINSS = 7786.12;
-        const percentualINSS = 0.11;
-        const descontoMaxINSS = 856.46;
-
-        let valorINSS = valorNF * percentualINSS;
-        if (valorNF > tetoINSS) {
-            valorINSS = descontoMaxINSS;
+        function toggleElements(type) {
+            const optionValue = document.querySelector(`input[name="${type}Option"]:checked`).value;
+            const elements = document.getElementById(`${type}Elements`);
+            if (optionValue === 'Sim') {
+                elements.classList.remove('hidden');
+            } else {
+                elements.classList.add('hidden');
+            }
         }
+        
 
-        const faixasIR = [
-            { limite: 2259.20, aliquota: 0, deducao: 0 },
-            { limite: 2826.65, aliquota: 0.075, deducao: 169.44 },
-            { limite: 3751.05, aliquota: 0.15, deducao: 381.44 },
-            { limite: 4664.68, aliquota: 0.225, deducao: 662.77 },
-            { limite: Infinity, aliquota: 0.275, deducao: 896 }
-        ];
+        function calcularImpostos() {
+            try {
+                const valorNota = parseFloat(document.getElementById('valorNota').value.replace(/\./g, '').replace(',', '.'));
+                const irrfOption = document.querySelector('input[name="irrfOption"]:checked').value;
+                const irrfAliquota = parseFloat(document.getElementById('irrfAliquota').value) / 100;
+                const issOption = document.querySelector('input[name="issOption"]:checked').value;
+                const issAlquota = parseFloat(document.getElementById('issAlquota').value) / 100;
+                const issBase = parseFloat(document.getElementById('issBase').value.replace(/\./g, '').replace(',', '.'));
+                const inssOption = document.querySelector('input[name="inssOption"]:checked').value;
+                const inssBase = parseFloat(document.getElementById('inssBase').value.replace(/\./g, '').replace(',', '.'));
+                const inssAliquota = parseFloat(document.getElementById('inssAliquota').value) / 100;
+                const pisCofinsCsllOption = document.querySelector('input[name="pisCofinsCsllOption"]:checked').value;
 
-        let valorDescontadoINSS = valorNF - valorINSS;
-        let valorIR = 0;
+                let pis = 0, cofins = 0, csll = 0;
+                if (pisCofinsCsllOption === 'Sim') {
+                    pis = valorNota * 0.0065;
+                    cofins = valorNota * 0.03;
+                    csll = valorNota * 0.01;
+                }
 
-        for (let i = 0; i < faixasIR.length; i++) {
-            if (valorDescontadoINSS <= faixasIR[i].limite) {
-                valorIR = (valorDescontadoINSS * faixasIR[i].aliquota) - faixasIR[i].deducao;
-                break;
+                let irrf = 0, inss = 0, iss = 0;
+                if (irrfOption === 'Sim') irrf = valorNota * irrfAliquota;
+                if (inssOption === 'Sim') inss = inssBase * inssAliquota;
+                if (issOption === 'Sim') iss = issBase * issAlquota;
+
+
+    const pisarred = Math.round(pis * 100) / 100;
+    const cofinsarred = Math.round(cofins * 100) / 100;
+    const csllarred = Math.round(csll * 100) / 100;
+    const irrfarred = Math.round(irrf * 100) / 100;
+    const inssarred = Math.round(inss * 100) / 100;
+    const issarred = Math.round(iss * 100) / 100;  
+    const valorFinal = valorNota - pisarred - cofinsarred - csllarred - irrfarred - inssarred - issarred;  
+
+    const resultado = 
+    `Valor PIS: R$ ${pisarred.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}<br>
+    Valor COFINS: R$ ${cofinsarred.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}<br>
+    Valor CSLL: R$ ${csllarred.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}<br>
+    Valor IRRF: R$ ${irrfarred.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}<br>
+    Valor INSS: R$ ${inssarred.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}<br>
+    Valor ISS: R$ ${issarred.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}<br><br>
+    Valor Final da Nota: R$ ${valorFinal.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
+
+            document.getElementById('resultado').innerHTML = resultado;
+            } catch (error) {
+            alert('Insira valores válidos.');
             }
         }
 
-        const valorLiquido = valorDescontadoINSS - valorIR;
-
-        const resultadoFinal = document.getElementById('resultado');
-        resultadoFinal.innerHTML = `<p>Valor INSS: R$ ${formatarNumero(valorINSS)}</p> <p>Valor IR: R$ ${formatarNumero(valorIR)}</p> <p>Valor Líquido: R$ ${formatarNumero(valorLiquido)}</p>`;
-
-        const imprimirBtn = document.getElementById('imprimirBtn');
-        imprimirBtn.style.display = 'block';
-        imprimirBtn.dataset.inss = valorINSS;
-        imprimirBtn.dataset.ir = valorIR;
-        imprimirBtn.dataset.liquido = valorLiquido;
-    }
-
-    function formatarNumero(numero) {
-        return numero.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-    }
-
-    function imprimirRelatorio() {
-        let valorNF = parseFloat(document.getElementById('ValorNF').value.replace(/\./g, '').replace(',', '.'));
-        let valorINSS = parseFloat(document.getElementById('imprimirBtn').dataset.inss);
-        let valorIR = parseFloat(document.getElementById('imprimirBtn').dataset.ir);
-        let valorLiquidoNota = parseFloat(document.getElementById('imprimirBtn').dataset.liquido);
-
-        let relatorio = `Relatório de Análise de Orçamento\n`;
-        relatorio += `Valor Nota: R$ ${formatarNumero(valorNF)}\n`;
-        relatorio += `Valor INSS: R$ ${formatarNumero(valorINSS)}\n`;
-        relatorio += `Valor IR: R$ ${formatarNumero(valorIR)}\n`;
-        relatorio += `Valor Líquido Nota: R$ ${formatarNumero(valorLiquidoNota)}\n`;
-
-        let relatorioWindow = window.open('', 'Relatório de Análise de Orçamento', 'width=600,height=400');
-        relatorioWindow.document.write('<pre>' + relatorio + '</pre>');
-        relatorioWindow.document.close();
-        relatorioWindow.print();
-    }
+        function formatNumber(number) {
+            return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        }
